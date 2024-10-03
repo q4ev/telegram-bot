b0f0b24186a605818cba02e0eba63fae3b704ee1
a.chernov <a.chernov@vozovoz.ru>
2024-10-03 10:47:51 +0300

    code & docs

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# telegram-bot
+Config file can look like this:
+
+```php
+return [
+    'db' => require __DIR__ . '/db.php',
+	'request' => [
+		// your settings
+	],
+	'response' => [
+		// your settings
+	],
+	'telegramBot' => [
+        'class' => 'qaev\telegram\TelegramBot',
+        // where to send
+        'url' => 'https://api.telegram.org/bot000000000:AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+        // where '000000000' is your bot's id
+        // and 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' token of your bot
+        // chats we're using
+        // u can use actual id, but in hindsight quick nicknames look easier
+        'chats' => [
+            // channels
+            '-1000000000000' => ['ch', 'channel'],
+            // personal
+            '100000000'  => ['me', 'spiderman'],
+            '90000000'   => ['mf', 'my-friend'],
+            '1000000000' => ['gf', 'his-girlfriend'],
+        ],
+        'contextOptions' => [ // options for file_get_contents (it's used for usual text sending)
+            'ssl' => [
+                'allow_self_signed' => true,
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+            ],
+        ],
+        'nicknameByDefault' => static fn () => \Yii::$app->params['tgBotDefaultSender'],
+         // or
+         // 'nicknameByDefault' => static fn () => 'me',	
+    ],
+];
+```
---
+++ telegram/TelegramBot.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace qaev\telegram\TelegramBot;
+
+
+use yii\base\InvalidArgumentException;
+use yii\base\InvalidConfigException;
+
+/**
+ * @property array $chats
+ */
+class TelegramBot extends \yii\base\BaseObject
+{
+	protected array $_chats = [];
+
+	public ?array $contextOptions = null;
+	public ?\Closure $nicknameByDefault = null;
+
+	public string $url;
+
+	protected function __prepareText ($text, bool $rawText = false)
+	{
+		if (null === $text)
+			return '/null/';
+
+		if (false === $text)
+			return '/false/';
+
+		if (true === $text)
+			return '/true/';
+
+		if ([] === $text)
+			return '/[]/';
+
+		if ('' === $text)
+			return '/String is empty/';
+
+		if (is_int($text))
+			return '/'.$text.'/';
+
+		$json = $rawText
+			? \urlencode($text)
+			: \json_encode($text, JSON_UNESCAPED_UNICODE);
+
+		if (false === $json)
+			throw new \Exception('Cannot JSON-encode '.$text);
+
+		if (mb_strlen($json, 'utf-8') > 4096)
+			$json = mb_substr($json, 0, 4090, 'utf-8').'~';
+
+		return $json;
+	}
+
+	public function getChat ($nickname)
+	{
+		// in case $nickname is not a nickname but already a valid chat id
+		if (preg_match('/^-?\d*$/', $nickname))
+			return $nickname;
+
+		// in case nickname omitted
+		if (!$nickname && $f = $this->nicknameByDefault)
+			$nickname = $f();
+
+		return $this->_chats[$nickname] ?? null;
+	}
+
+	public function getChats (): array
+	{
+		return $this->_chats;
+	}
+
+	public function send ($text, ?string $to = null, bool $rawText = false)
+	{
+		$json = $this->__prepareText($text, $rawText);
+
+		$result = [];
+		foreach ((array)$to as $nickname)
+		{
+			if (!$chatId = $this->getChat($nickname))
+				throw new InvalidArgumentException("Receiver's chat for $nickname not found");
+
+			$context = stream_context_create($this->contextOptions);
+
+			$result[$nickname] = file_get_contents(
+				$this->url."/sendMessage?chat_id=$chatId&parse_mode=html&text=$json",
+				false,
+				$context
+			);
+		}
+
+		if (!is_array($to))
+			return reset($result);
+
+		return $result;
+	}
+
+	public function sendPhoto ($filePath, ?string $to = null, ?string $caption = null)
+	{
+		$result = [];
+
+		$cFile = new \CURLFile($filePath);
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $this->url.'/sendPhoto');
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		foreach ((array)$to as $nickname)
+		{
+			if (!$chatId = $this->getChat($nickname))
+				throw new InvalidArgumentException("Receiver's chat for $nickname not found");
+
+			// putting data
+			$post = [
+				'chat_id' => $chatId,
+				'photo' => $cFile,
+			];
+			if ($caption)
+				$post['caption'] = $caption;
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+
+			$result[$nickname] = curl_exec($ch);
+		}
+
+		curl_close($ch);
+
+		if (!is_array($to))
+			return reset($result);
+
+		return $result;
+	}
+
+	public function setChats (array $chats)
+	{
+		foreach ($chats as $chatId => $nicknames)
+		{
+			if (!is_array($nicknames))
+				throw new InvalidConfigException('Nicknames for chats must be an array');
+
+			foreach ($nicknames as $nickname)
+			{
+				if ($this->_chats[$nickname])
+					throw new InvalidConfigException("Nickname '$nickname' used more than once");
+
+				$this->_chats[$nickname] = $chatId;
+			}
+		}
+	}
+}
